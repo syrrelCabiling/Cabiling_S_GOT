8e0db5f0f142130dc2bc61aa2642f5330e37b974
Syrrel Cabiling <syrrelcabiling@Syrrels-MacBook-Pro.local>
2018-04-14 20:09:52 -0400

    finalizing js

--- js/main.js
+++ js/main.js
@@ -2,23 +2,32 @@
   console.log('video stuff loaded');
 
   //custom function on object prototype
-  String.prototype.capIt = function() { return this.replace(this.charAt(),
+  String.prototype.capIt = function() {return this.replace(this.charAt(),
   this.charAt().toUpperCase()); }
 
 
   //variables come first
-  let sigils = document.querySelectorAll('.sigilContainer'),
-      lightbox = document.querySelector('.lightbox'),
-      closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
-      vidPlayer = document.querySelector('video'),
-	  vidControls = document.querySelector('.controls'),
-	  imageBanner = document.querySelector('#houseImages');
+//  let sigils = document.querySelectorAll('.sigilContainer'),
+//      lightbox = document.querySelector('.lightbox'),
+//      closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
+//      vidPlayer = document.querySelector('video'),
+//	  vidControls = document.querySelector('.controls'),
+//	  imageBanner = document.querySelector('#houseImages'),
+//	  //houseDef = document.querySelector('#house-info');
+let sigils = document.querySelectorAll('.sigilContainer'),
+                lightbox = document.querySelector('.lightbox'),
+                closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
+                vidPlayer = document.querySelector('video'),
+                vidControls = document.querySelector('.controls'),
+                imageBanner = document.querySelector('#houseImages'),
+				houseText = document.querySelector('.house-info').value ;
 
-//Functions in the middle
+	//Functions in the middle
 
-function scrollBanners(offset) {
-		let moveIt = offset * 600 + "px";
-		 imageBanner.style.right = moveIt;
+	function scrollBanners(offset) {
+			let moveIt = offset * 600 + "px";
+		 	imageBanner.style.right = moveIt;
+		scrollBanners(this.dataset.offset);
 	}  
 	
 function showHouseVideo() {
@@ -26,16 +35,22 @@ function showHouseVideo() {
 //split apart the class name on the element, grab the house from the result
   document.querySelector('h1').textContent = `House ${houseName}`;
   //debugger;
- // lightbox.classList.add('show-lightbox');
+lightbox.classList.add('show-lightbox');
   //make it play -->(take autoplay off in index.html)
   vidPlayer.src = `videos/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
   vidPlayer.load();
   vidPlayer.play();
-  
-	scrollBanners(this.data.offset);
-}
+  scrollBanners(this.dataset.offset);
 
+}
 	
+
+//houseText = document.querySelector('p').value 
+	//var houseText = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+//document.querySelector('p').textContent = `${houseText}`;
+function houseDef(){
+	houseText.classList.add('house-info');
+}
 	
 function closeLightbox() {
   //debugger;
@@ -65,7 +80,8 @@ sigils.forEach(sigil => sigil.addEventListener('click', showHouseVideo));
 closeLightBoxButton.addEventListener('click', closeLightbox);
 vidPlayer.addEventListener('ended', closeLightbox);
 vidControls.addEventListener('click',pausePlay);
-imageBanner.addEventListener('click',moveIt)
+imageBanner.addEventListener('click',scrollBanners);
+houseText.addEventListener('click',scrollBanners);
 
 })();
 
